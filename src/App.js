63c3464ecb2785ba8ef64b05b2cd98d0492c11d4
xJkit <xjkit54780@gmail.com>
2018-05-22 00:58:38 +0800

    Add ch7ex3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Canvas from './Canvas';
+/** Containers */
+import Home from './containers/Home';
+import BasicExample from './containers/BasicExample';
+import Ch7Ex3 from './containers/Ch7Ex3';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
@@ -13,7 +17,38 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Canvas Demo</h1>
         </header>
-        <Canvas />
+        <div className="main">
+          <ul className="sider">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/basic-example">
+                Basic Example
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/ch7-ex3">
+                CH7 EX3
+              </NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route
+              exact
+              path="/"
+              component={Home}
+            />
+            <Route
+              path="/basic-example"
+              component={BasicExample}
+            />
+            <Route
+              path="/ch7-ex3"
+              component={Ch7Ex3}
+            />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/containers/Ch7Ex3/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import mp3 from './song1.wav';
+
+class Ch7Ex3 extends React.Component {
+  audio = null;
+  canvas = null;
+  ctx = null;
+
+  componentDidMount() {
+    this.canvas = document.getElementById('the-canvas');
+    this.audio = document.getElementById('the-audio');
+    this.ctx = this.canvas.getContext('2d');
+
+    // load audio
+    this.audio.addEventListener('canplaythrough', this.renderCanvas, false);
+    this.audio.load();
+  }
+
+  componentWillUnmount() {
+    this.audio.removeEventListener('canplaythrough', this.renderCanvas, false);
+  }
+
+  renderCanvas = () => {
+    /* background */
+    this.ctx.fillStyle = '#222';
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.strokeStyle = "#eee";
+    this.ctx.strokeRect(5,  5, this.canvas.width-10, this.canvas.height-10);
+    // render text
+    this.renderCanvasText();
+
+    // play audio
+
+    requestAnimationFrame(this.renderCanvas);
+  }
+
+  renderCanvasText = () => {
+    const audioElement = this.audio;
+    this.ctx.fillStyle = '#eee';
+    this.ctx.font = '24px serif';
+    this.ctx.fillText("Current time:" + audioElement.currentTime,  20 ,50);
+    this.ctx.fillText("Loop: " + audioElement.loop,  20 ,80);
+    this.ctx.fillText("Autoplay: " +audioElement.autoplay,  20 ,110);
+    this.ctx.fillText("Muted: " + audioElement.muted,  20 ,140);
+    this.ctx.fillText("Controls: " + audioElement.controls,  20 ,170);
+    this.ctx.fillText("Volume: " + audioElement.volume,  20 ,200);
+    this.ctx.fillText("Paused: " + audioElement.paused,  20 ,230);
+    this.ctx.fillText("Ended: " + audioElement.ended,  20 ,260);
+    this.ctx.fillText("Source: " + audioElement.currentSrc,  20 ,290);
+    this.ctx.fillText("Can Play OGG: " + audioElement.canPlayType("audio/ogg"),  20 ,320);
+    this.ctx.fillText("Can Play WAV: " + audioElement.canPlayType("audio/wav"),  20 ,350);
+    this.ctx.fillText("Can Play MP3: " + audioElement.canPlayType("audio/mp3"),  20 ,380);
+    this.ctx.fillText("Duration:" + audioElement.duration,  20 ,410);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>CH7EX3 : Audio Properties And The Canvas</h2>
+        <audio
+          id="the-audio"
+          src={mp3}
+          controls
+        />
+        <canvas
+          id="the-canvas"
+          width="700"
+          height="500"
+        />
+      </div>
+    );
+  }
+}
+
+export default Ch7Ex3;
